867fa49f84371eba34c0fd969dc5d13a7ddabf05
RyanZurrin <ryanzurrin@ryanzurrin.com>
2023-05-05 15:27:46 -0400

    got it all working and adding linkedin scraper third party module

--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -3,24 +3,16 @@ import requests
 
 
 def scrape_linkedin_profile(linkedin_profile_url: str):
-    """scrape information from LinkedIn profiles,
-    Manually scrape the information from the LinkedIn profile"""
+    """ Scrape information from a LinkedIn profiles.
+    Manually scrape the information from a LinkedIn profile.
+
+    :param linkedin_profile_url: The url of the LinkedIn profile.
+    :return:  A dictionary of the scraped information.
+    """
     api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
-    header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
+    header_dic = {"Authorization": f'Bearer {os.environ["PROXYCURL_API_KEY"]}'}
 
     response = requests.get(
         api_endpoint, params={"url": linkedin_profile_url}, headers=header_dic
     )
 
-    data = response.json()
-    data = {
-        k: v
-        for k, v in data.items()
-        if v not in ([], "", "", None)
-        and k not in ["people_also_viewed", "certifications"]
-    }
-    if data.get("groups"):
-        for group_dict in data.get("groups"):
-            group_dict.pop("profile_pic_url")
-
-    return data
