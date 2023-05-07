c765b9a2dc5066c110baeca5de714be0a14005e9
RyanZurrin <ryanzurrin@ryanzurrin.com>
2023-05-07 17:02:09 -0400

    added tools and agents modules and updated ice_breaker to scrape linkedin

--- tools/tools.py
+++ tools/tools.py
@@ -4,7 +4,11 @@ from langchain.agents import tool
 
 @tool
 def get_profile_url(text: str) -> str:
-    """Searches for Linkedin Profile Page."""
+    """ Searches for Linkedin Profile page url in text.
+
+    :param text: The text to extract the url from.
+    :return: The url of the LinkedIn profile.
+    """
     search = SerpAPIWrapper()
     res = search.run(f"{text}")
     return res
